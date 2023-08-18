b0f31b819fa2154b31f3e0b2c01f10b900d4c86a
Mohannad-AlDardiri <mhnd62864@gmail.com>
2023-08-18 09:20:06 +0300

    fixing routes issue

--- src/index.js
+++ src/index.js
@@ -7,9 +7,8 @@ import App from "./Components/App";
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-        <App />
-      </Router>
+      <App />
+    </Router>
   </React.StrictMode>,
-
   document.getElementById("root")
 );
